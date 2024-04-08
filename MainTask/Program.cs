3b35152056b62e39c4fe149ec70c85dce05d6293
Daniel Gote <gothierlaw@gmail.com>
2024-04-08 21:32:36 +0200

    Создал функцию проверки условия

---
+++ MainTask/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] FilterArray(string[] arr)
+{
+    int count = 0;
+    foreach (string str in arr)
+    {
+        if(str.Length <= 3)
+        {
+            count++;
+        }
+    }
+}
+
